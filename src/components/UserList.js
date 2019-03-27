0b97e275aff8a3eade77d816c87e72b20180c135
gwilliam187 <33857496+gwilliam187@users.noreply.github.com>
2019-03-27 14:21:29 +0100

    updated view to accommodate RxDB

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,7 @@ class UserList extends Component {
 	renderList() {
 		return this.props.users.map(user => {
 			return (
-				<option value={ user }>{ user }</option>
+				<option key={ user } value={ user }>{ user }</option>
 			);
 		});
 	}
@@ -18,6 +18,7 @@ class UserList extends Component {
 						<div className='form-group w-100 mb-0'>
 							<div className='text-left w-25'><label className='d-inline'>User</label></div>
 							<select className='form-control w-75'>
+								<option disabled selected value> -- select a user -- </option>
 								{ this.renderList() }
 							</select>
 						</div>
