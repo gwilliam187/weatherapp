14402834b315dcd29cc0175a4a8b9e3cb6d54f69
Leonardo Theoson <thefatflyer@gmail.com>
2019-04-01 17:02:53 +0200

    solved steven_klarens bumping into david_thomas issue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,11 +6,13 @@ import weathersReducer from './weathersReducer';
 import citiesErrorMessageReducer from './citiesErrorMessageReducer';
 import userListReducer from './userListReducer';
 import selectedUserReducer from './selectedUserReducer';
+import loginReducer from './loginReducer';
 
 export default combineReducers({
 	users: userListReducer,
 	selectedUser: selectedUserReducer,
 	cities: citiesReducer,
+	loginState: loginReducer,
 	selectedCities: selectedCitiesReducer,
 	weathers: weathersReducer,
 	citiesErrorMessage: citiesErrorMessageReducer
