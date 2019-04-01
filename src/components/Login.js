7be2207d9328ed71f39759ffa6f5309fc17c8a57
gwilliam187 <33857496+gwilliam187@users.noreply.github.com>
2019-04-01 14:24:46 +0200

    fixed some css

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,19 @@ import React from 'react'
 import UserInput from './UserInput';
 
 class Login extends React.Component{
-    render(){
-        return (
-            <div className="container app">
+	render(){
+		return (
+			<div className="container app">
 				<div className='row flex-grow-1'>
-                    <div className='col-12 d-flex'>
-                        <div className='row flex-grow-1 align-self-end'>
-                            <UserInput />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+					<div className='col-12 d-flex'>
+						<div className='row flex-grow-1 align-self-center'>
+							<UserInput />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Login;
