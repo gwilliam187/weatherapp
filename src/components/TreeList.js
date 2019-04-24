3ec4a90b1ab73460eb4f4105895b86d84e70a2d0
Leonardo Theoson <thefatflyer@gmail.com>
2019-04-24 10:24:17 +0200

    fixed tree styling

--- src/components/TreeList.js
+++ src/components/TreeList.js
@@ -6,12 +6,12 @@ class TreeList extends React.Component{
 		if	(this.props.trees.length>0){
 			return this.props.trees.map(tree=>{
 				return(
-					<div className='d-flex border-top border-bottom' key={tree._id}>
+					<div className='d-flex border-top border-bottom tree-list' key={tree._id}>
 						<div
-							className='mr-auto city-button-main'
+							className='mr-auto tree-list-main'
 							onClick={ this.handleCityClick }>
 							<div className='name'>{ tree.description }</div>
-							<div className='ref'>{ tree._id n}</div>
+							<div className='ref'>{ tree._id }</div>
 						</div>
 					</div>
 				)
@@ -26,7 +26,7 @@ class TreeList extends React.Component{
 	render() {
 		return (
 			<div className='col-12 card mb-3'>
-				<div className='card-body'>
+				<div className='card-body tree-list'>
 					<label>Trees</label>
 					{ this.renderList() }
 				</div>
